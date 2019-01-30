6f0e32ae52a908acf64f2eeaebdb2ca2c928bebd
ayusuf358 <yusuf90abu@yahoo.com>
2019-01-29 21:57:36 -0500

    My First Tutorial

---
+++ src/org/tutorial/test1/Tutorial1.java
@@ -0,0 +1,20 @@
+/**
+ * This is for Java learning
+ */
+package org.tutorial.test1;
+
+/**
+ * @author yusuf
+ *
+ */
+public class Tutorial1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world testing one to one");
+
+	}
+
+}
