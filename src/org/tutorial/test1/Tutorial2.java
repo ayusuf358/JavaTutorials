f6c9c0304fac3c3573831ecd08beba8317a53d32
ayusuf358 <yusuf90abu@yahoo.com>
2019-01-31 21:59:43 -0500

    Another change

---
+++ src/org/tutorial/test1/Tutorial2.java
@@ -0,0 +1,20 @@
+/**
+ * This is for Java learning
+ */
+package org.tutorial.test1;
+
+/**
+ * @author yusuf
+ *
+ */
+public class Tutorial2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my second program");
+
+	}
+
+}
